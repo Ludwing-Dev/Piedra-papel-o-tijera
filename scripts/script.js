a17dd1d7c2c77a20dcad4e71ef0d08be3c0084bc
Ludwing-Dev <moiseslovera2019@gmail.com>
2026-03-22 17:24:04 -0400

    implement playRound logic and real-time score tracking

--- scripts/script.js
+++ scripts/script.js
@@ -9,20 +9,44 @@ function getComputerChoice () {
     }
 }
 
-
 function getHumanChoice () {
-    let valorUsuario = prompt('Ingresa "piedra, papel o tijera"');
+    let valorUsuario = prompt("Ingresa: piedra, papel o tijera");
     if (valorUsuario !== null && valorUsuario.trim() !== "") {
         valorUsuario = valorUsuario.toLowerCase();
     } else {
         return;
     }
     if (valorUsuario == "piedra" || valorUsuario == "papel" || valorUsuario == "tijera") {
-            return valorUsuario;
-        } else {
-            return alert("Ingresa un valor válido")
+        return valorUsuario;
+    } else {
+        return alert("Ingresa un valor válido");
         }
 }
 
-getHumanChoice()
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
 
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice !== undefined && humanChoice !== null) {
+        if (humanChoice == computerChoice) {
+            console.log(`Empate. Ambos eligieron ${computerChoice}`);
+        } else if (
+            (humanChoice == "piedra" && computerChoice == "papel") ||
+            (humanChoice == "papel" && computerChoice == "tijera") ||
+            (humanChoice == "tijera" && computerChoice == "piedra")
+        ) {
+            computerScore++;
+            console.log(`¡Perdiste!. ${computerChoice} vence a ${humanChoice}. Marcador: Jugador ${humanScore} - Máquina ${computerScore}`);
+        } else {
+            humanScore++;
+            console.log(`¡Ganaste!. ${humanChoice} vence a ${computerChoice}. Marcador: Jugador ${humanScore} - Máquina ${computerScore}`);
+        }
+    } else {
+        return;
+    }
+}
+
+playRound(humanSelection, computerSelection);
